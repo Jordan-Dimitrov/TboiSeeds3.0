37a8af835d8e5cd56d088544a9598cc032847fc4
Jordan-Dimitrov <iordan.dimitrov.highschool@svetlina.net>
2024-06-12 22:29:53 +0300

    finished auth

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,34 @@
 import express, { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import User from './models/user';
+import session, { Session } from 'express-session';
+import dotenv from 'dotenv';
 
 const app = express();
+
+interface CustomSession extends Session {
+  userId?: string;
+}
+
+app.use(session({
+  secret: process.env.SECRET || '',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+  }
+}));
+
+const requireAuth = (req: Request, res: Response, next: any) => {
+  const session = req.session as CustomSession;
+  if (session.userId) {
+    next();
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+};
 
 app.use(express.json());
 
@@ -28,7 +54,8 @@ app.post('/register', async (req: Request, res: Response) => {
 
         res.json(user);
       }
-    }).catch(error => res.status(500).send('Internal server error'))
+    })
+    .catch(error => res.status(500).send('Internal Server error'));
 });
 
 app.post('/login', (req, res, next) => {
@@ -36,7 +63,7 @@ app.post('/login', (req, res, next) => {
 
   User.findOne({
     where: {
-      username: req.body.username
+      username: username
     }
   })
   .then(user => {
@@ -44,8 +71,9 @@ app.post('/login', (req, res, next) => {
       res.status(401).send('No such user found');
     }
     else {
-      bcrypt.compare(req.body.password, user.password, (error, result) => {
+      bcrypt.compare(password, user.password, (error, result) => {
         if(result) {
+          (req.session as CustomSession).userId = '{user.id}';
           res.json(user);
         }
         else {
@@ -54,11 +82,12 @@ app.post('/login', (req, res, next) => {
         }
       })
     }
-  }).catch(error => res.status(500).send('Internal server error'))
+  })
+  .catch(error => next(error));
 })
 
 
-app.get('/users', async (req: Request, res: Response) => {
+app.get('/users', requireAuth, async (req: Request, res: Response) => {
   try {
     const users = await User.findAll();
     res.json(users);
